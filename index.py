4721facdb1a3eb8f0ac6e1f7408f86609ee59f5f
LOGAn0007 <128821528+LOGAn0007@users.noreply.github.com>
2025-04-09 11:35:19 +0530

    Delete index.py

--- index.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-if __name__ == '__main__':
-    model = YOLO('yolov8n.yaml')  # Replace 'yolov8n.pt' with your model if needed
-    results = model.train(data="config.yaml", epochs=100, imgsz=640)
